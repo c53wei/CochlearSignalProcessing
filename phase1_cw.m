49f170f720fd7fb9cdfb164202365f85986d79b8
Clare Wei <c53wei@edu.uwaterloo.ca>
2021-06-11 18:17:04 -0400

    Resampling and stereo-->mono conversion

---
+++ phase1_cw.m
@@ -0,0 +1,29 @@
+% Define audio files to process
+audiofiles = ["moving_sound.m4a" "background_noise.m4a"];
+for i = 1:length(audiofiles)
+    % Perform QA on files if necessary
+    formatSound(audiofiles(i));
+end 
+
+function [y, Fs] = formatSound(filename)
+% Reads audio file, resampling and converting to mono if needed
+
+% Extract basename
+[~,name,~] = fileparts(filename);
+[y,Fs] = audioread(filename);
+% Resample to 16 kHz if needed
+if Fs ~= 16000
+    Fsnew = 16e3;
+    [P,Q] = rat(Fsnew/Fs);
+    y = resample(y,P,Q);
+    Fs = Fsnew;
+end
+% If stereo, convert to mono
+[m, n] = size(y);
+if n == 2
+    % Sum across rows
+    y = sum(y, 2);
+end
+% Write resampled file
+audiowrite(strcat(name, '_resampled.wav'),y,Fs);
+end
