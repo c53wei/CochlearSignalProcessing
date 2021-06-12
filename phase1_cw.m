e99796ae0a604dd45e15b03ee5ab6ad127bfce03
Clare Wei <c53wei@edu.uwaterloo.ca>
2021-06-12 10:02:01 -0400

    Cell function to CosineCreator for Task 3.7

--- phase1_cw.m
+++ phase1_cw.m
@@ -1,3 +1,6 @@
+%% Helper function
+cosineHelper = @(y, Fs, filename) createCosine(y, Fs, 1000, filename);
+%% Task 3.1-3.6 (Signal Reformatting)
 % Define audio files to process
 audiofiles = ["moving_sound.m4a" "background_noise.m4a"];
 % Container to store all matrices by applying function to list
@@ -6,3 +9,7 @@ audiofiles = ["moving_sound.m4a" "background_noise.m4a"];
 % Creates waveform plots for each audiofile
 cellfun(@plotWaveform, signal_data, num2cell(audiofiles));
 
+%% Task 3.7 Generating cosine for each file
+cos_sound = cellfun(cosineHelper, signal_data, Fs_data, ...
+    num2cell(audiofiles), 'UniformOutput', false);
+% sound(cos_sound{1}, 1000} % Play it back if you'd like
