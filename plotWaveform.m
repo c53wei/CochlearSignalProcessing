76a441046b75e65c7624158917c9c7667d649f94
Clare Wei <c53wei@edu.uwaterloo.ca>
2021-06-12 10:00:22 -0400

    Comments + formatting

--- plotWaveform.m
+++ plotWaveform.m
@@ -3,6 +3,6 @@ function plotWaveform(signal_values, filename)
 [~, name, ~] = fileparts(filename);
 plot(signal_values);
 xlabel('Sample Number');
-ylabel('Value (Units?)');
-saveas(gcf, strcat(name, '.png'))
+ylabel('Value (Units?)'); % Not sure what the unit is
+saveas(gcf, strcat(name, '.png'));
 end
