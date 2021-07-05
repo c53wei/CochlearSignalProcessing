9d7d45d21f6d82ab299a0d8cdb63da7c3e2fc1a0
Clare Wei <c53wei@edu.uwaterloo.ca>
2021-07-05 13:49:21 -0400

    Restructured filtering to be called all at once

---
+++ Rachel's Stuff/phase2_main.m
@@ -0,0 +1,28 @@
+%% Task 5: Filter sounds with pass bank
+
+% Define audio files to process
+audiofiles = ["../moving_sound_resampled.wav" "../background_noise_resampled.wav"];
+[signal_data, Fs_data] = arrayfun(@(x) audioread(x), audiofiles, ...
+    'UniformOutput', false);
+% Define bandwidth intervals from 100 Hz to 8 kHz – Feel free to modify
+bw = {[100 500] [500 700] [700 900] [900 1100] [1100 1300] ...
+    [1300 1800] [1800 2800] [2800 4800] [4800 8000]};
+% Creates list of filter functions that can be called later 'in parallel'
+filters = cellfun(@bandPass, bw);
+
+% For each audio file, run each filter interval on it
+for i=1:length(audiofiles)
+    % Isolate audio file of interest
+    signal = signal_data{i};
+    % Separate audio file into frequencies of interest
+    split_band = cellfun(@filter, num2cell(filters), ...
+        repmat({signal}, 1, length(filters)), 'UniformOutput', false);
+    % Rectify each frequency
+    split_band = cellfun(@abs, split_band, 'UniformOutput', false);
+    % Plot lowest and highest frequency bandwidths
+    [~, name, ~] = fileparts(audiofiles(i));
+    plot(split_band(1))
+    xlabel('Amplitude');
+    ylabel('Sample Number');
+    title(strcat(num2str(bw{1}), 'Hz'));
+end
